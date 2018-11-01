4d370e4678347c62ec570ec4989fc207b1812662
Jensen Judkins <thesmelliestman@gmail.com>
2018-11-01 09:47:26 -0600

    Drew a turtle aka a triangle. and now I drew a peace sign

--- CreativityProject/View/DrawingView.swift
+++ CreativityProject/View/DrawingView.swift
@@ -17,6 +17,9 @@ public class DrawingView : UIView
     public  override func draw(_ rect: CGRect) -> Void
     {
         // Drawing code
+        createStickFigure().stroke()
+        drawTurtle()
+        peaceSign().stroke()
     }
     
     
@@ -24,8 +27,63 @@ public class DrawingView : UIView
     {
         let figure : UIBezierPath = UIBezierPath()
         
+        UIColor.brown.setStroke()
+        UIColor.brown.setFill()
+        figure.lineWidth = 4.0
+        
+        figure.addArc(withCenter: CGPoint(x: 200, y: 200),
+                      radius: CGFloat(20),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x: 200, y: 270))
+        figure.move(to: CGPoint(x: 180, y: 240))
+        figure.addLine(to: CGPoint(x: 220, y: 240))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 180, y: 300))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
+        figure.close()
+        figure.fill()
+        
+        
         
         return figure
+    }
+    
+    private func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.green.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
+    }
+    
+    private func peaceSign() -> UIBezierPath
+    {
+        let peace : UIBezierPath = UIBezierPath()
+        
+        UIColor.gray.setStroke()
+        peace.lineWidth = 3.0
+        peace.addArc(withCenter: CGPoint(x: 75, y: 75),
+                      radius: CGFloat(50),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        peace.move(to: CGPoint(x: 75, y: 75))
+        peace.addLine(to: CGPoint(x: 75, y: 25))
+        peace.move(to: CGPoint(x: 75, y: 75))
+        peace.addLine(to: CGPoint(x: 25, y: 95))
+        peace.move(to: CGPoint(x: 75, y: 75))
+        peace.addLine(to: CGPoint(x: 125, y: 95))
+        
+        
+        
+        return peace
     }
 
 }
