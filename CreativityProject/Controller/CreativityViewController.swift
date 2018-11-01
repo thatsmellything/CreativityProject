e5410afa0f6f5457ce31139e1c991af1e558c606
Jensen Judkins <thesmelliestman@gmail.com>
2018-11-01 08:31:03 -0600

    Created a button that when clicked switches to a blank screen

--- CreativityProject/Controller/CreativityViewController.swift
+++ CreativityProject/Controller/CreativityViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class CreativityViewController: UIViewController {
 
-    override func viewDidLoad() {
+   public override func viewDidLoad() -> Void
+   {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
